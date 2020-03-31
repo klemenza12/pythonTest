3e1090cb1ea9f92e1c7147b5add197f419f32d25
klemenza12@gmail.com <hristochernev@Hristos-MacBook-Pro.local>
2020-03-31 13:50:43 +0200

    start with excersices

---
+++ classes.py
@@ -0,0 +1,30 @@
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+# print(p1.x)
+
+class Person:
+    def __init__ (self, name, age):
+        self.name = name
+        self.age = age
+
+    def myFunc (self):
+        print (self.name + " " + str(self.age)  + " this is the name of the person who was called and the age of the person")
+
+p2 = Person ("John", 36)
+print (p2.name, p2.age) 
+p2.myFunc()
+
+class Person2:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.age)
+
+p1 = Person2("Ivan", 36)
+p1.name = "Venelin"
+del p1.age
+p1.myfunc()
