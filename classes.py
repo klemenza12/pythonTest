a96fbc3fba38a88e51532ff46bf73e49515c4cf0
klemenza12@gmail.com <hristochernev@Hristos-MacBook-Pro.local>
2020-03-31 13:56:01 +0200

    more

--- classes.py
+++ classes.py
@@ -17,12 +17,12 @@ print (p2.name, p2.age)
 p2.myFunc()
 
 class Person2:
-  def __init__(mysillyobject, name, age):
-    mysillyobject.name = name
-    mysillyobject.age = age
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
 
-  def myfunc(abc):
-    print("Hello my name is " + abc.age)
+  def myfunc(self):
+    print("Hello my name is " + self.age)
 
 p1 = Person2("Ivan", 36)
 p1.name = "Venelin"
