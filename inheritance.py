4ced70aef8d09a04b251e253c99523b688c398b6
klemenza12@gmail.com <hristochernev@Hristos-MacBook-Pro.local>
2020-04-10 17:40:47 +0200

    continue

--- inheritance.py
+++ inheritance.py
@@ -8,12 +8,23 @@ class Person:
 
 
 class Student(Person):
-    def __init__(self, fname, lname):
-        Person.__init__(self, fname, lname)
+    def __init__(self, large, long):
+        Person.__init__(self, large, long)
 
+
+class Pedal(Student):
+    def __init__(self, first, second, year):
+        super().__init__(first, second)
+        self.graduationyear = year
+    
+    def welcome(self):
+        print(str(self.graduationyear) + " " + str(self.firstname) + " " + str(self.lastname))
 
 
 x = Student("John", "Doe")        
 x.printname()
 
 
+y = Pedal("Georgi", "Doe", 2020)        
+y.printname()
+y.welcome()
