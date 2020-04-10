844f4240a768f5d5a3bc49d9c85477eec6389cb7
klemenza12@gmail.com <hristochernev@Hristos-MacBook-Pro.local>
2020-04-10 17:19:42 +0200

    we continue the excercises

---
+++ inheritance.py
@@ -0,0 +1,19 @@
+class Person:
+    def __init__ (self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+
+    def printname (self):
+        print (self.firstname + " " + self.lastname)
+
+
+class Student(Person):
+    def __init__(self, fname, lname):
+        Person.__init__(self, fname, lname)
+
+
+
+x = Student("John", "Doe")        
+x.printname()
+
+
